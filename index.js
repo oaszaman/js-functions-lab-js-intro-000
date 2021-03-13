ece89ff6b584e9a391aeff556f2323c9a1c77a55
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-13 00:28:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo() {
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo() {
+  return  `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown() {
+  return `It's ${days} days until ${holiday}!`
+}
